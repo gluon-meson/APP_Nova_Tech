f79ca36777ca8082c62ac511582f199709dae1eb
fayanLiao <fayan.liao@thoughtworks.com>
2024-04-08 15:40:29 +0800

    feat: modify mock file array

--- app/api/query/mockData.ts
+++ app/api/query/mockData.ts
@@ -1,16 +1,46 @@
 import { type File } from './home'
 
-export const uploadFiles: File[] = [
+const files: File[] = [
   {
     id: '1',
-    name: 'Earning report',
+    name: 'Oil and Gas Sector Update_030124_Others.pdf',
   },
   {
     id: '2',
-    name: 'Regulatory changes',
+    name: 'Oil and Gas Sector Update_200224_Others.pdf',
   },
   {
     id: '3',
-    name: 'Strategic partnership',
+    name: 'Oil and Gas Sector Update_280324_Others.pdf',
+  },
+  {
+    id: '4',
+    name: 'Marco Strategy_China_040324__Others.pdf',
+  },
+  {
+    id: '5',
+    name: 'Global Strategy_200324_Others.pdf',
+  },
+  {
+    id: '6',
+    name: 'Schwab_Cisco.pdf',
+  },
+  {
+    id: '7',
+    name: 'Schwab_Booking.pdf',
+  },
+  {
+    id: '8',
+    name: 'Schwab_Westlake.pdf',
+  },
+  {
+    id: '9',
+    name: 'Schwab_homeDepot.pdf',
+  },
+  {
+    id: '10',
+    name: 'Schwab_CocaCola.pdf',
   },
 ]
+
+export const uploadFiles: File[] = files
