5d0aad50c6a60f86144c82914a8103b434f11371
levisburn <qiang.liang@thoughtworks.com>
2024-04-18 11:55:02 +0800

    feat[#143]: change GPT_MODEL

--- features/chat-bot/openaiClient.ts
+++ features/chat-bot/openaiClient.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai'
 
-export const GPT_MODEL = 'gpt-4-1106-preview' //'gpt-4-turbo-preview' gpt-3.5-turbo gpt-4 gpt-4-1106-preview
+export const GPT_MODEL = 'gpt-4-turbo-2024-04-09' //'gpt-4-turbo-preview' gpt-3.5-turbo gpt-4 gpt-4-1106-preview
 
 export const getOpenaiClient: () => OpenAI = (function () {
   let instance: OpenAI
